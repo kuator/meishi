e9761d528591aa82e59b194567748bca98e85223
Kuat <kuatabylkasymov@gmail.com>
2020-03-23 16:51:33 +0600

    update

--- script.js
+++ script.js
@@ -1,81 +1,106 @@
-let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('myCanvas'));
-let context = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const width = canvas.width;
-const height = canvas.height;
-let colors = [ '#900C3E', '#C70039', '#FF5733', '#FFC300'];
+(function(){
+  let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('myCanvas'));
+  let context = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  let colors = [ '#900C3E', '#C70039', '#FF5733', '#FFC300'];
 
-class MyAnimation {
-  constructor(number){
-    this.number = number;
-    this.circles = [];
-    this.start = null;
-    this.setup();
-  }
 
-  setup(){
-    let radius = 50;
-    let spawn = {};
-    let velocity = {};
-    let randomVelocity = 1;
-    for(let i = 0; i<this.number; i++){
-      let color = colors[Math.floor(Math.random()*colors.length)];
-      spawn.x = Math.random()*(width-2*radius) + radius; 
-      spawn.y = Math.random()*(height-2*radius) + radius; 
-      let x = Math.random()*randomVelocity - randomVelocity/2; 
-      let y = Math.random()*randomVelocity - randomVelocity/2; 
-      this.circles.push(new Circle(radius, spawn.x, spawn.y, x, y, color));
+  class MyAnimation {
+    constructor(number){
+      this.number = number;
+      this.circles = [];
+      this.start = null;
+      this.setup();
+      this.raf = null;
+      this.isRafPlaying = false;
+    }
+
+    startAnimation(){
+      if(!this.isRafPlaying){
+        this.raf = window.requestAnimationFrame(timestamp => this.step(timestamp));
+        this.isRafPlaying = true;
+      } else {
+        cancelAnimationFrame(this.raf);
+        this.raf = window.requestAnimationFrame(timestamp => this.step(timestamp));
+      }
+    }
+
+    setup(){
+      let radius = 50;
+      let spawn = {};
+      let velocity = {};
+      let randomVelocity = 1;
+      for(let i = 0; i<this.number; i++){
+        let color = colors[Math.floor(Math.random()*colors.length)];
+        spawn.x = Math.random()*(canvas.width-2*radius) + radius; 
+        spawn.y = Math.random()*(canvas.height-2*radius) + radius; 
+        let x = Math.random()*randomVelocity - randomVelocity/2; 
+        let y = Math.random()*randomVelocity - randomVelocity/2; 
+        this.circles.push(new Circle(radius, spawn.x, spawn.y, x, y, color));
+      }
+    }
+
+    step(timestamp){
+      this.update();
+      this.render();
+      this.raf = window.requestAnimationFrame(timestamp => this.step(timestamp));
+    }
+
+    update(){
+      for(let i = 0; i<this.number; i++){
+        this.circles[i].update();
+      }
+    }
+
+    render(){
+      context.clearRect(0,0, canvas.width, canvas.height);
+      for(let i = 0; i<this.number; i++){
+        this.circles[i].render();
+      }
     }
   }
 
-  step(timestamp){
-    this.update();
-    this.render();
-    window.requestAnimationFrame(timestamp => this.step(timestamp));
-  }
+  class Circle {
+    constructor(radius, x, y, speedX, speedY, color){
+      this.radius = radius;
+      this.x = x;
+      this.y = y;
+      this.speedX = speedX;
+      this.speedY = speedY;
+      this.color = color;
+    }
 
-  update(){
-    for(let i = 0; i<this.number; i++){
-      this.circles[i].update();
+    update(){
+      if(this.x + this.radius + this.speedX>= canvas.width  || this.x - this.radius + this.speedX <= 0)
+        this.speedX = -this.speedX;
+      if(this.y + this.radius + this.speedY>= canvas.height || this.y - this.radius + this.speedY <= 0)
+        this.speedY = -this.speedY;
+      this.x = this.x + this.speedX;
+      this.y = this.y + this.speedY;
+    }
+
+    render(){
+      context.beginPath();
+      context.fillStyle = this.color;
+      context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+      context.fill(); 
     }
   }
 
-  render(){
-    context.clearRect(0,0, width, height);
-    for(let i = 0; i<this.number; i++){
-      this.circles[i].render();
-    }
-  }
-}
-
-
-class Circle {
-  constructor(radius, x, y, speedX, speedY, color){
-    this.radius = radius;
-    this.x = x;
-    this.y = y;
-    this.speedX = speedX;
-    this.speedY = speedY;
-    this.color = color;
+  let animation = new MyAnimation(10);
+  window.addEventListener('resize', resizeCanvas, false);
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drawStuff();
   }
 
-  update(){
-    if(this.x + this.radius + this.speedX>= width  || this.x - this.radius + this.speedX <= 0)
-      this.speedX = -this.speedX;
-    if(this.y + this.radius + this.speedY>= height || this.y - this.radius + this.speedY <= 0)
-      this.speedY = -this.speedY;
-    this.x = this.x + this.speedX;
-    this.y = this.y + this.speedY;
+  function drawStuff(){
+    animation.update();
+    animation.startAnimation();
   }
+  resizeCanvas();
 
-  render(){
-    context.beginPath();
-    context.fillStyle = this.color;
-    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    context.fill(); 
-  }
-}
 
-let animation = new MyAnimation(10);
-window.requestAnimationFrame(timestamp => animation.step(timestamp));
+})();
